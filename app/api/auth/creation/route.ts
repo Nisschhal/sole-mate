bb8a88d96c8fd60c25331f79a076b60c9e2afe7f
Nisschhal <mrnischalpuri@gmail.com>
2024-11-08 11:45:53 +0545

    postinstall prisma in script due to next 14 version

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,61 +1,24 @@
-// import prisma from "@/app/lib/db";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const { getUser } = getKindeServerSession();
-//   const user = await getUser();
-
-//   // if no user throw error
-//   if (!user || user == null || !user.id) {
-//     throw new Error("Something went wrong in user auth creation...");
-//   }
-
-//   // check if user exist with the id
-//   let dbUser = await prisma.user.findUnique({
-//     where: {
-//       id: user.id,
-//     },
-//   });
-
-//   // if doesn't, create a new user
-//   if (!dbUser) {
-//     dbUser = await prisma.user.create({
-//       data: {
-//         id: user.id,
-//         firstName: user.given_name ?? "",
-//         lastName: user.family_name ?? "",
-//         email: user.email ?? "",
-//         profileImage:
-//           user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-//       },
-//     });
-//   }
-
-//   // return to the given url, url must be absolute path
-//   return NextResponse.redirect("http://localhost:3000");
-// }
-
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
-import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
-  noStore();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    throw new Error("Something went wrong...");
+  // if no user throw error
+  if (!user || user == null || !user.id) {
+    throw new Error("Something went wrong in user auth creation...");
   }
 
+  // check if user exist with the id
   let dbUser = await prisma.user.findUnique({
     where: {
       id: user.id,
     },
   });
 
+  // if doesn't, create a new user
   if (!dbUser) {
     dbUser = await prisma.user.create({
       data: {
@@ -69,10 +32,6 @@ export async function GET() {
     });
   }
 
-  // return NextResponse.redirect(
-  //   process.env.NODE_ENV === "development"
-  //     ? "http://localhost:3000/"
-  //     : "https://shoe-marshal.vercel.app/"
-  // );
+  // return to the given url, url must be absolute path
   return NextResponse.redirect("http://localhost:3000");
 }
