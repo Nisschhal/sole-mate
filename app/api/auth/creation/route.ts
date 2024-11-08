7ed600e7d388ca35c47d435aa0721ece0730a0a8
Nisschhal <mrnischalpuri@gmail.com>
2024-11-08 11:40:15 +0545

    auth/creation api check

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,6 +1,6 @@
-import prisma from "@/app/lib/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextResponse } from "next/server";
+// import prisma from "@/app/lib/db";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { NextResponse } from "next/server";
 
 // export async function GET() {
 //   const { getUser } = getKindeServerSession();
@@ -36,45 +36,43 @@ import { NextResponse } from "next/server";
 //   return NextResponse.redirect("http://localhost:3000");
 // }
 
+import prisma from "@/app/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
+
 export async function GET() {
-  try {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    // Log the user object for debugging purposes
-    console.log(user);
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong...");
+  }
 
-    // if no user, throw error
-    if (!user || !user.id) {
-      throw new Error("Something went wrong in user auth creation...");
-    }
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
 
-    // check if user exists in the database
-    let dbUser = await prisma.user.findUnique({
-      where: {
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
         id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
-
-    if (!dbUser) {
-      dbUser = await prisma.user.create({
-        data: {
-          id: user.id,
-          firstName: user.given_name ?? "",
-          lastName: user.family_name ?? "",
-          email: user.email ?? "",
-          profileImage:
-            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-        },
-      });
-    }
-
-    // return the response
-    return NextResponse.redirect("http://localhost:3000");
-  } catch (error) {
-    console.error("Error during API call:", error); // Log any error that occurs
-    throw new Error(
-      "An error occurred during user creation or authentication."
-    );
   }
+
+  // return NextResponse.redirect(
+  //   process.env.NODE_ENV === "development"
+  //     ? "http://localhost:3000/"
+  //     : "https://shoe-marshal.vercel.app/"
+  // );
+  return NextResponse.redirect("http://localhost:3000");
 }
